e382af38d5f846c1fb625f3c8a2a3e4a3b8fe218
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-08 20:30:58 -0500

    Add twiter gem and pry, sandboxing twitter ruby obj

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source 'https://rubygems.org'
 gem 'sinatra'
+gem 'twitter'
+gem 'pry'
--- server.rb
+++ server.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'twitter'
+require 'pry'
+
+config = {
+  consumer_key: ENV["TWITTER_CONSUMER_KEY"],
+  consumer_secret: ENV["TWITTER_CONSUMER_SECRET"]
+}
+
 
 get '/' do
-  "hello world"
+  client = Twitter::REST::Client.new(config)
+  puts "*" * 30
+  puts client.user('gp3gp3gp3')
+  binding.pry
+  puts "*" * 30
 end
