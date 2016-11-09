2f69d97e3eed1924d818dece5c7f43a40cb4f3ef
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-08 22:07:47 -0500

    Add twitter users collection, send back json, no FB auth yet

--- server.rb
+++ server.rb
@@ -1,17 +1,45 @@
 require 'sinatra'
 require 'twitter'
 require 'pry'
+require 'json'
 
-config = {
-  consumer_key: ENV["TWITTER_CONSUMER_KEY"],
-  consumer_secret: ENV["TWITTER_CONSUMER_SECRET"]
-}
+TWITTER_USERNAMES = [
+  'VancityReynolds',
+  'DAVID_LYNCH',
+  'ConanOBrien',
+  'prattprattpratt',
+  'BillGates',
+  'StephenAtHome',
+  'elonmusk',
+  'rickygervais',
+  'BarackObama',
+  'kanyewest'
+]
 
+def twitter_client
+  config = {
+    consumer_key: ENV["TWITTER_CONSUMER_KEY"],
+    consumer_secret: ENV["TWITTER_CONSUMER_SECRET"]
+  }
+  Twitter::REST::Client.new(config)
+end
+
+def get_twitter_user(user_name)
+  user_hash = {}
+  client = twitter_client
+  twitter_obj = client.user(user_name)
+  user_hash['user_name'] = twitter_obj.screen_name
+  user_hash['followers_count'] = twitter_obj.followers_count
+  user_hash['friends_count'] = twitter_obj.friends_count
+  user_hash
+end
 
 get '/' do
-  client = Twitter::REST::Client.new(config)
-  puts "*" * 30
-  puts client.user('gp3gp3gp3')
-  binding.pry
-  puts "*" * 30
+  stream_hash = {}
+  twitter_users = TWITTER_USERNAMES.map do |user_name|
+    get_twitter_user(user_name)
+  end
+  stream_hash['twitter'] = twitter_users
+  content_type :json
+  stream_hash.to_json
 end
